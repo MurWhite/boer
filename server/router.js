885520e4e4717521c5d65a4ebc33ebee794b3a57
白波 <baibo@meituan.com>
2017-07-05 15:48:41 +0800

    push for server test

--- server/router.js
+++ server/router.js
@@ -5,16 +5,24 @@
 const kRouter = require('koa-router');
 const send = require('koa-send');
 const path = require('path');
-const configer = require('../server.config')
+const configer = require('../server.config');
+const marked = require('marked');
 
-var router = new kRouter();
+let router = new kRouter();
 
+// static server
 router.get(['/static/', '/static/:filename'], async (ctx) => {
   let filename = ctx.params.filename || 'index.html';
   await send(ctx, filename, {root: configer.static_path})
 });
 router.get('/static/:path/:filename', async (ctx) => {
   await send(ctx, `${ctx.params.path}/${ctx.params.filename}`, {root: configer.static_path})
+});
+
+// markdown 解析
+
+router.get('/post/:postname', async (ctx)=>{
+  let postname = ctx.params.postname || 'post-sample';
 });
 
 module.exports = router;
